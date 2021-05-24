966ff2c2a297de5341f0627f74cd2682771857ba
ibnuubaidah <ibnuubaidah22@gmail.com>
2021-05-24 22:13:10 +0700

    create a framework

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
